0b638fae5f5a499c08f05a5842848a5fb3840805
Anna <alexsander.stetsenko@itechcraft.com>
2021-08-03 21:33:00 +0300

    first commit

---
+++ src/main/java/pageObjects/pageObject.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class pageObject extends BasePage{
+
+    public pageObject(WebDriver driver) {
+        super(driver);
+    }
+}
